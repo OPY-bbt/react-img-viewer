97a109baa4469282da4c2845514365e06d647c21
zhangyan <279375210@qq.com>
2019-02-15 18:34:08 +0800

    feat(develop): add pan handler in zoom precess

--- src/react-img-viewer.tsx
+++ src/react-img-viewer.tsx
@@ -14,20 +14,6 @@ enum Action { NONE, SWIPE, PINCH, MOVE }
 
 type TouchEvent = React.TouchEvent;
 
-type Props = {
-  visible: boolean;
-  imgs: string[];
-  prevSrc: string;
-  mainSrc: string;
-  nextSrc: string;
-  onNext: () => void;
-  onPrev: () => void;
-  children?: never;
-  title: string;
-} & typeof defaultProps;
-
-type State = typeof initialState;
-
 const defaultProps = Object.freeze({});
 
 const initialState = Object.freeze({
@@ -43,11 +29,25 @@ const initialState = Object.freeze({
   keyIndex: 0,
   nextKeyIndex: 0,
 
-  isZooming: false,
+  needImgTransition: false,
 });
 
+type Props = {
+  visible: boolean;
+  imgs: string[];
+  prevSrc: string;
+  mainSrc: string;
+  nextSrc: string;
+  onNext: () => void;
+  onPrev: () => void;
+  children?: never;
+  title: string;
+} & typeof defaultProps;
+
+type State = typeof initialState;
+
 class ReactImgViewer extends React.Component<Props, State> {
-  static readonly defaultProps = {};
+  static readonly defaultProps = defaultProps;
 
   static getTransform({ x = 0, y = 0, zoom = 1 }) {
     return {
@@ -66,11 +66,19 @@ class ReactImgViewer extends React.Component<Props, State> {
   endPosY: number = 0;
   endTime: number = 0;
 
+  startOffsetImgX: number = 0;
+  startOffsetImgY: number = 0;
+
+  endOffsetImgX: number = 0;
+  endOffsetImgY: number = 0;
+
   lastClickTime: number = 0;
 
   currentAction: Action = Action.NONE;
 
   refContainer: React.RefObject<HTMLDivElement> = React.createRef();
+
+  refCurrentImg: React.RefObject<HTMLImageElement> = React.createRef();
 
   componentDidMount() {
     this.refContainer.current.addEventListener("touchmove",
@@ -86,6 +94,28 @@ class ReactImgViewer extends React.Component<Props, State> {
       bottom: 0,
       left: 0,
     };
+  }
+
+  getImgNaturalSizes = (img: HTMLImageElement): number[] => {
+    return [
+      img.naturalWidth,
+      img.naturalHeight,
+    ];
+  }
+
+  getRealImgSizes = (sizes: number[]): number[] => {
+    const { width, height } = this.getViewerRect();
+    const [imgW, imgH] = sizes;
+
+    const maxRatio = width / height;
+    const srcRatio = imgW / imgH;
+
+    let imgSizes = [width, imgH * width / imgW];
+    if (srcRatio < maxRatio) {
+      imgSizes = [imgW * height / imgH, height];
+    }
+
+    return imgSizes.concat((height - imgSizes[1]) / 2, (width - imgSizes[0]) / 2);
   }
 
   handleTouchStart = (event: TouchEvent) => {
@@ -128,22 +158,32 @@ class ReactImgViewer extends React.Component<Props, State> {
   }
 
   handleMoveOrSwipeStart = (event: TouchEvent) => {
-    const { zoomLevel } = this.state;
+    const { zoomLevel, offsetImgX, offsetImgY } = this.state;
     const touch = event.targetTouches[0];
+
     this.currentAction = zoomLevel <= MIN_ZOOM_LEVEL ?
       Action.SWIPE : Action.MOVE;
     this.startPosX = this.endPosX = touch.clientX;
     this.startPosY = this.endPosY = touch.clientY;
     this.startTime = this.endTime = event.timeStamp;
+
+    this.startOffsetImgX = offsetImgX;
+    this.startOffsetImgY = offsetImgY;
   }
 
   handlePanMove = (event: TouchEvent) => {
     const touch = event.targetTouches[0];
 
+    const offsetImgX = this.startOffsetImgX + touch.clientX - this.startPosX;
+    const offsetImgY = this.startOffsetImgY + touch.clientY - this.startPosY;
+
     this.setState({
-      offsetImgX: touch.clientX - this.startPosX,
-      offsetImgY: touch.clientY - this.startPosY,
+      offsetImgX,
+      offsetImgY,
     });
+
+    this.endOffsetImgX = offsetImgX;
+    this.endOffsetImgY = offsetImgY;
 
     this.endPosX = touch.clientX;
     this.endPosY = touch.clientY;
@@ -151,6 +191,27 @@ class ReactImgViewer extends React.Component<Props, State> {
   }
 
   handlePanMoveEnd = (event: TouchEvent) => {
+    const { zoomLevel } = this.state;
+    const [realWidth, realHeight, paddingTop, paddingLeft] =
+      this.getRealImgSizes(this.getImgNaturalSizes(this.refCurrentImg.current));
+
+    // TODO: 需要考虑图片没加载获取不到宽高的情况
+    const maxOffsetImgX = realWidth * (zoomLevel - 1) / 2 - paddingLeft;
+    const maxOffsetImgY = realHeight * (zoomLevel - 1) / 2 - paddingTop;
+
+    if (Math.abs(this.endOffsetImgX) > maxOffsetImgX) {
+      this.setState({
+        offsetImgX: this.endOffsetImgX > 0 ? maxOffsetImgX : -maxOffsetImgX,
+      });
+    }
+
+    if (Math.abs(this.endOffsetImgY) > maxOffsetImgY) {
+      this.setState({
+        offsetImgY: this.endOffsetImgY > 0 ? maxOffsetImgY : -maxOffsetImgY,
+      });
+    }
+
+    // 处理单击或者双击事件
     this.handleClick(event);
     this.currentAction = Action.NONE;
   }
@@ -171,8 +232,16 @@ class ReactImgViewer extends React.Component<Props, State> {
     this.setState({
       zoomLevel: zoomLevel === MIN_ZOOM_LEVEL ?
         MAX_ZOOM_LEVEL : MIN_ZOOM_LEVEL,
-      isZooming: true,
+      needImgTransition: true,
+      offsetImgX: 0,
+      offsetImgY: 0,
     });
+
+    // 防止对下次双击放大滑动有影响
+    this.startOffsetImgX = 0;
+    this.startOffsetImgY = 0;
+    this.endOffsetImgX = 0;
+    this.endOffsetImgY = 0;
   }
 
   handleSwipeMove = (event: TouchEvent) => {
@@ -182,7 +251,6 @@ class ReactImgViewer extends React.Component<Props, State> {
     if (isAnimating) {
       return;
     }
-
     this.setState({
       offsetX: touch.clientX - this.startPosX,
     });
@@ -247,13 +315,21 @@ class ReactImgViewer extends React.Component<Props, State> {
 
   handleImgTransitionEnd = () => {
     this.setState({
-      isZooming: false,
+      needImgTransition: false,
     });
   }
 
   handlePinchStart = (event: TouchEvent) => {
     // const [touch1, touch2] = event.targetTouches;
     // this.currentAction = Action.PINCH;
+    // const { offsetX } = this.state;
+
+    // if (offsetX !== 0) {
+    //   this.handleMoveOrSwipeStart(event);
+    // } else {
+    //   console.log(123);
+    // }
+    // handleMoveOrSwipeStart
   }
 
   render() {
@@ -266,7 +342,7 @@ class ReactImgViewer extends React.Component<Props, State> {
       offsetX, offsetY,
       offsetImgX, offsetImgY,
       isAnimating, nextKeyIndex, keyIndex,
-      zoomLevel, isZooming,
+      zoomLevel, needImgTransition,
     }  = this.state;
 
     if (!visible) {
@@ -288,7 +364,7 @@ class ReactImgViewer extends React.Component<Props, State> {
         x: offsetImgX,
         y: offsetImgY,
       }),
-      transition: isZooming ? `transform .3s` : "none",
+      transition: needImgTransition ? `transform .3s` : "none",
     };
 
     const imgs = (
@@ -296,6 +372,7 @@ class ReactImgViewer extends React.Component<Props, State> {
         <div key={`${img}-${keyIndex + idx - 1}`} className="riv-img__container">
           <img
             className="riv-img"
+            ref={idx === 1 ? this.refCurrentImg : undefined}
             src={img}
             style={idx === 1 ? imgStyle : {}}
             onTransitionEnd={this.handleImgTransitionEnd}
